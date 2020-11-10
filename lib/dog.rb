ad7c330d241adfc6d00b3d1ab2a10b26da5a2b6b
stacy <stacydn23@gmail.com>
2020-11-09 20:30:50 -0500

    commit tree 4ce36ab6d0e933e3abe3244015ea9370cc2d95b9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        #@@all << self
+        self.save
+    
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def self.clear_all
+        self.all.clear
+    end 
+
+    def self.print_all
+        all_dogs = self.all
+        all_dogs.each do |dogs| puts dogs.name
+        end 
+    end 
+
+    def save 
+        @@all << self
+    end 
+end 
